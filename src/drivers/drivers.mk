ef1013905df9613b671322412bc0e7a3f92a9ee7
Santosh Sajjan <ssajjan@codeaurora.org>
2011-11-18 13:27:21 +0530

    Added support for MSM driver CMD.
    Driver specific commands (STOP/START/SETPOWER/GETPOWER) support is added.
    Also enable NL80211 support in wpa_supplicant and hostapd.
    
    Change-Id: I4ecca395e547f3e3f5141a524b2414e9103faebb

--- src/drivers/drivers.mk
+++ src/drivers/drivers.mk
@@ -38,6 +38,7 @@ endif
 ifdef CONFIG_DRIVER_NL80211
 DRV_CFLAGS += -DCONFIG_DRIVER_NL80211
 DRV_OBJS += src/drivers/driver_nl80211.c
+DRV_OBJS += src/drivers/driver_qcom_cmd_nl80211.c
 DRV_OBJS += src/utils/radiotap.c
 NEED_SME=y
 NEED_AP_MLME=y
