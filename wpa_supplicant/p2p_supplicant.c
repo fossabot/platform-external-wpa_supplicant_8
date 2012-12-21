7d1ace9e25df9531b06aa509c92d489af0c87423
Jouni Malinen <jouni@qca.qualcomm.com>
2012-12-21 11:42:00 +0530

    p2p_cancel did not properly cancel a pending p2p_connect-join operation.
    Address the different steps in that process: initial scan, Provision
    Discovery exchange before connection, and WPS provisioning step
    (including the scans before WPS).
    
    Change-Id: I957b2a701e079d5d2ba8bfe016f01cab3a7695e8
    (cherry picked from commit 9ce05e5080ffcbf9917ab4bc800c3ad36e180d6b)

--- wpa_supplicant/p2p_supplicant.c
+++ wpa_supplicant/p2p_supplicant.c
@@ -3412,6 +3412,7 @@ static int wpas_p2p_join_start(struct wpa_supplicant *wpa_s)
 	}
 
 	group->p2p_in_provisioning = 1;
+	wpa_s->global->p2p_group_formation = wpa_s;
 	group->p2p_fallback_to_go_neg = wpa_s->p2p_fallback_to_go_neg;
 
 	os_memset(&res, 0, sizeof(res));
@@ -5162,6 +5163,12 @@ void wpas_p2p_update_channel_list(struct wpa_supplicant *wpa_s)
 	p2p_update_channel_list(wpa_s->global->p2p, &chan);
 }
 
+static void wpas_p2p_scan_res_ignore(struct wpa_supplicant *wpa_s,
+					struct wpa_scan_results *scan_res)
+{
+	wpa_printf(MSG_DEBUG, "P2P: Ignore scan results");
+}
+
 
 int wpas_p2p_cancel(struct wpa_supplicant *wpa_s)
 {
@@ -5183,6 +5190,18 @@ int wpas_p2p_cancel(struct wpa_supplicant *wpa_s)
 		wpa_printf(MSG_DEBUG, "P2P: Unauthorize pending GO Neg peer "
 			   MACSTR, MAC2STR(peer));
 		p2p_unauthorize(global->p2p, peer);
+		found = 1;
+	}
+
+	if (wpa_s->scan_res_handler == wpas_p2p_scan_res_join) {
+		wpa_printf(MSG_DEBUG, "P2P: Stop pending scan for join");
+		wpa_s->scan_res_handler = wpas_p2p_scan_res_ignore;
+		found = 1;
+	}
+
+	if (wpa_s->pending_pd_before_join) {
+		wpa_printf(MSG_DEBUG, "P2P: Stop pending PD before join");
+		wpa_s->pending_pd_before_join = 0;
 		found = 1;
 	}
 
