88909fb6f8fee5cd5d9eb3a12ee8f5203405d2f4
Mingcheng Zhu <mingchen@codeaurora.org>
2015-05-10 21:25:43 -0700

    hostapd: Hack to build hostapd with enlarged timeout value
    Hack to build hostapd with enlarged timeout value
    
    Change-Id: I020690b6427f3fcbad6c4a260987b86c2276d325
    CRs-Fixed: 835304

--- hostapd/Android.mk
+++ hostapd/Android.mk
@@ -24,6 +24,9 @@ L_CFLAGS += -DVERSION_STR_POSTFIX=\"-$(PLATFORM_VERSION)\"
 # Set Android log name
 L_CFLAGS += -DANDROID_LOG_NAME=\"hostapd\"
 
+#This is a hack to increase timeout value for hostapd for Adrastea emu
+L_CFLAGS += -DQCA_WIFI_3_0_EMU_HOSTAPD
+
 # Disable unused parameter warnings
 L_CFLAGS += -Wno-unused-parameter
 
