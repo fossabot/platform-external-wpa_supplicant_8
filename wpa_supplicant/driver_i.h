3256c9b619baca6545af29d2aad94fbe5b5417e2
Kanchanapally, Vidyullatha <vkanchan@qti.qualcomm.com>
2016-06-17 19:19:13 +0530

    nl80211: Use extended capabilities per interface type
    This adds the necessary changes to support extraction and use of the
    extended capabilities specified per interface type (a recent
    cfg80211/nl80211 extension). If that information is available,
    per-interface values will be used to override the global per-radio
    value.
    
    CRs-Fixed: 1030694
    Git-commit: cc9a2575cabd2adb975df3677c6df5829f4279ad
    Git-repo : git://w1.fi/srv/git/hostap.git
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    
    Change-Id: Ia192693d606ebf9b7028ecc58be01cbf37cb4620

--- wpa_supplicant/driver_i.h
+++ wpa_supplicant/driver_i.h
@@ -902,4 +902,15 @@ static inline int wpa_drv_set_prob_oper_freq(struct wpa_supplicant *wpa_s,
 	return wpa_s->driver->set_prob_oper_freq(wpa_s->drv_priv, freq);
 }
 
+static inline int wpa_drv_get_ext_capa(struct wpa_supplicant *wpa_s,
+				       enum wpa_driver_if_type type)
+{
+	if (!wpa_s->driver->get_ext_capab)
+		return -1;
+	return wpa_s->driver->get_ext_capab(wpa_s->drv_priv, type,
+					    &wpa_s->extended_capa,
+					    &wpa_s->extended_capa_mask,
+					    &wpa_s->extended_capa_len);
+}
+
 #endif /* DRIVER_I_H */
